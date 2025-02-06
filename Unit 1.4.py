f69aeff241e4087d78e6e9c8726087f551579f94
JJ-778364 <778364@pdsb.net>
2025-02-06 12:30:42 -0500

    Create Unit 1.4.py

---
+++ Unit 1.4.py
@@ -0,0 +1,5 @@
+print("Use any whole number except zero...")
+num = input("Please input a whole number: ") 
+num = int(num) 
+num2 = num / 3.141596
+print("When", num, " is divided by 21374987", "the result is:", num2)
