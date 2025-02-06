e9c030fc34ee71d502639ff8489d9222d5e1a679
JJ-778364 <778364@pdsb.net>
2025-02-06 12:20:32 -0500

    Create Unit 1.3.py

---
+++ Unit 1.3.py
@@ -0,0 +1,4 @@
+print("Welcome to Sanjay's Koala Party!")
+print("Sanjay has 6 bowls for 144 cookies")
+print("There are ", 6, " people in this room.")
+print("The number of cookies per bowl is %d" % (144/6))
