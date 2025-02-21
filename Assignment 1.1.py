4f7f594b04153c88a2e02ce7210cc75e1dbe9daa
JJ-778364 <778364@pdsb.net>
2025-02-21 13:40:24 -0500

    Update Assignment 1.1.py

--- Assignment 1.1.py
+++ Assignment 1.1.py
@@ -1,24 +1,12 @@
 import math
 print("Welcome to the product even/odd detector!!")
 n1 = input("Please input your first number: ")
-n1 = int(n1)
+n1 = float(n1)
 n2 = input("Please input your second number: ")
-n2 = int(n2)
-if n1 % 2 == 0:
-  m1 = 2
-else:
-  m1 = 3
-  if n2 % 2 == 0:
-    m2 = 2
-  else:
-    m2 = 3
-    if m1 * m2 == 4:
-      m1 == 4
-    if m1 * m2 == 6:
-      m1 == 4
-    else:
-      m1 == 9
-      if m1 == 4:
-        print("Your number is ODD!")
-      else:
-        print("Your number is EVEN!!")
+n2 = float(n2)
+if (n1 % 2 == 0) and (n2 % 2 == 0):
+  print("Your number is EVEN!")
+elif (n1 % 2 > 0) and (n2 % 2 == 0):
+  print("Your number is EVEN!")
+elif (n1 % 2 > 0) and (n2 % 2 > 0):
+  print("Your number is ODD!")
