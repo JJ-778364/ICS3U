5219eac8f196ce870ff0b1be2d408c9385002439
JJ-778364 <778364@pdsb.net>
2025-03-25 13:37:33 -0400

    Update Unit 2.4.py

--- Unit 2.4.py
+++ Unit 2.4.py
@@ -1,12 +1,18 @@
-z = input("Give me a value of n: ")
-z = int(z)
-n = 1
-x = 1
-y = 1
-while (y <= z):
- x = n * (n + 1)
- x = x/2
- x = int(x)
- print(x)
- n = n + 1
- y = y + 1
+n = input("Give me a value of n: ")
+n = int(n)
+j = 0
+t = 0
+f = 0
+p = 1
+print("Counting from j = 1 to", n, ":")
+print("    j      tri         factorial")
+while j<n:
+  j = j + 1
+  j = int(j)
+  t = j * (j + 1)
+  t = t/2
+  t = int(t)
+  f = f + 1
+  p = p * f
+  p = int(p)
+  print("     ", j, "    ", t, "         ", p)
