280b0c145f8da4a026abe431c61039204fb68f7f
JJ-778364 <778364@pdsb.net>
2025-03-26 12:51:21 -0400

    Create Unit 3.1.py

---
+++ Unit 3.1.py
@@ -0,0 +1,9 @@
+progname = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
+print(progname)
+for c in progname:
+    print(c, sep="", end="")
+print()
+for c in range(len(progname)):
+    print(c, progname[c])
+for c in range(len(progname)):
+  
