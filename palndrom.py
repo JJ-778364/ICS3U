290104e87d185efc63b90f25b549ade7e59fd857
JJ-778364 <778364@pdsb.net>
2025-04-22 08:24:45 -0400

    Update palndrom.py

--- palndrom.py
+++ palndrom.py
@@ -4,13 +4,17 @@ Jenna Jawarneh
 ICS3U
 
 Variable Dictionairy:
-
+words = The list of words we want to check for palindromes
 """
+# Title to describe what the program does.
+print("Welcome to the Palindrome Program! This program will determine if a word is a palindrome.")
 
-print("Palindrome program!")
-words = ["racecar", "noon", "desk", "civic", "store", "rotor", "madam", "kayak", "school", "refer"]
-for pali in range(len(words)):
-    word = words[pali]
+# The list of words that we want to check for palindromes in an array, set to the variable "words".
+words = ["racecar", "noon", "park", "civic", "store", "rotor", "play", "kayak", "class", "refer"]
+
+# A loop that checks every index of the list, which is every word i
+for i in range(len(words)):
+    word = words[i]
     palindrome = True
     for x in range(len(word) // 2):
         if word[x] != word[len(word) - 1 - x]:
