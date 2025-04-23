f3ccdda4406427e54b776635188c2830a7523027
JJ-778364 <778364@pdsb.net>
2025-04-23 12:16:18 -0400

    Update palndrom.py

--- palndrom.py
+++ palndrom.py
@@ -4,7 +4,10 @@ Jenna Jawarneh
 ICS3U
 
 Variable Dictionairy:
-words = The list of words we want to check for palindromes
+words = The list of words beig checked for palindromes
+word = Every word in the list
+Palindrome = set to either True or False to determine if the word is a palindrom or not
+
 """
 # Title to describe what the program does.
 print("Welcome to the Palindrome Program! This program will determine if a word is a palindrome.")
@@ -22,16 +25,25 @@ for i in range(len(words)):
     # The code assumes the words are not palindromes, until proven true.
     palindrome = False
     
-    # This loop turns every word into an array, dividing the number of characters by 2 and rounding down
+    # This loop turns every word into an array, dividing the number of characters by 2 and rounding down.
     for x in range(len(word) // 2):
         
-        # This if statement checks if every letter is the same as its corresponding 
-        # letter in the second half of the word.
-        # Using the length of the word, then subtracting 1, as loops start at 0, 
+        # This if statement checks if every letter in the first half of the word is the same as its  
+        # corresponding letter in the second half of the word.
+        # It checks by using the length of the word, then subtracting 1 to account for the loop starting
+        # at 0, then subtracting the index. For example with the word "racecar", it calculates first if
+        # r = the letter at 7 - 1 - 0, which is the r again.
+        # a = the letter at 7 - 1 - 1 which is a, and so on.
         if word[x] == word[len(word) - 1 - x]:
+            
+            # If the previous condition applies, it sets the palindrome as true.
             palindrome = True
+
+    # If it was proven as true, it prints this statement.
     if palindrome:
         print(word, "is a palindrome")
+    
+    # In any other scenario, it will be false, meaning the word is not a palindrome.
     else:
         print(word, "is not a palindrome")
         
