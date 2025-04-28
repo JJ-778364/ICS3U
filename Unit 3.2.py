d9a2669911624c18a7354764946c8b674c94cda9
JJ-778364 <778364@pdsb.net>
2025-04-28 12:39:00 -0400

    Update Unit 3.2.py

--- Unit 3.2.py
+++ Unit 3.2.py
@@ -1,1 +1,10 @@
-
+list = []
+x = input("How many items are you entering?")
+x = int(x)
+print("Enter your items below:")
+for i in range(x):
+  item = input("Item name:")
+  list.append(item)
+print("You have entered", x, "items.")
+for x in list:
+  print(x)
