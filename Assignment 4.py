a610a4ff8ef8b8a88255ade966135250b7934dc8
JJ-778364 <778364@pdsb.net>
2025-05-12 12:20:02 -0400

    Create Assignment 4.py

---
+++ Assignment 4.py
@@ -0,0 +1,16 @@
+print("Welcome to the Wordle Database!!!")
+x = input("Enter W if searching for a word and D if searching for a date: ")
+if x == "W":
+  word = input("Enter your word: ")
+if x == "D":
+  year = input("Enter the year: ")
+  month = input("Enter the month: ")
+  day = input("Enter the day: ")
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+for i in range(months):
+  numMonth = month[i] + 1
+
+def date(month: str, day: str, year: str) -> int:
+  
+def search(word: str)
